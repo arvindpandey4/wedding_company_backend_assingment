e0caafb6f83aa56091122ac2b7f92cedcb16ff9e
Arvind Pandey <114512882+arvindpandeysrm@users.noreply.github.com>
2025-12-08 16:54:09 +0530

    chore: initializing application

--- app.py
+++ app.py
@@ -1,32 +1,5 @@
 from flask import Flask, render_template_string, request
+import random
 app = Flask(__name__)
 
-INDEX = """
-<!doctype html><title>UC1 - Snake & Ladder</title>
-<h1>UC1 - Single Player start</h1>
-<p>Player starts at position 0</p>
-<form method="post" action="/play">
-  <button type="submit">Show start position</button>
-</form>
-"""
 
-RESULT = """
-<!doctype html><title>UC1 Result</title>
-<pre>
-Total Dice Roll: 0
-Player: Player 1
-Position: 0
-</pre>
-<p><a href="/">Back</a></p>
-"""
-
-@app.route("/", methods=["GET"])
-def index():
-    return render_template_string(INDEX)
-
-@app.route("/play", methods=["POST"])
-def play():
-    return render_template_string(RESULT)
-
-if __name__ == "__main__":
-    app.run(debug=True)
